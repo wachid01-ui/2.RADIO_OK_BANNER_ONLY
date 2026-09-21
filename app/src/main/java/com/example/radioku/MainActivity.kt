0817b701616fa58d369e4f96f2154d9f5668a825
wachid01-ui <wachid01@gmail.com>
2026-09-21 13:10:52 +0700

    Remove duplicate import statements in MainActivity.kt

--- app/src/main/java/com/example/radioku/MainActivity.kt
+++ app/src/main/java/com/example/radioku/MainActivity.kt
@@ -10,6 +10,12 @@ import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
